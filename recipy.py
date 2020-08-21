2940e59997256b91dc5ce59a5558f81aaa503945
Lukas Neugebauer <l.neugebauer@uke.de>
2020-08-21 15:36:51 +0200

    Should be a bit more flexible now

--- recipy.py
+++ recipy.py
@@ -6,7 +6,9 @@ import requests
 import os
 import webbrowser
 import argparse
-    
+import re
+
+
 def main(url, *args, **kwargs):
     parser = _get_html_parser(url)
     tag = _get_recipe_tag(parser)
@@ -17,19 +19,21 @@ def main(url, *args, **kwargs):
     )
     _open_html(html)
 
+
 def _get_html_parser(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def _write_html(recipe, file = '', folder = None):
+
+def _write_html(recipe, file='', folder=None):
     if file:
-        file = '_'.join(recipe['title'].split( ))
+        file = '_'.join(recipe['title'].split())
     if not file.endswith('.html'):
         file += '.html'
     if folder is None:
         folder = '/tmp'
-    filename = os.path.join(folder,file)
+    filename = os.path.join(folder, file)
     scaffolding = """<html>
     <head>
     <title>{}</title>
@@ -39,16 +43,24 @@ def _write_html(recipe, file = '', folder = None):
     {}
     </body>
     </html> 
-    """.format(recipe['title'], recipe['title'],'{}')
+    """.format(recipe['title'], recipe['title'], '{}')
     ingredients = (
         "<h2>Ingredients</h2>" + 
         "\n".join(["<p>{}</p>".format(x) for x in recipe['ingredients']])
     ) if 'ingredients' in recipe.keys() else ""
-    instructions =(
+    instructions = (
         "<h2>Instructions</h2>" + 
     "\n".join(["<h4>{}</h4>\n<p>{}</p>".format(1+i,x) for i,x in enumerate(recipe['instructions'])])
     ) if 'instructions' in recipe.keys() else ""
-    image = "<img src=\"{}\">".format(recipe['images'][0]) if 'images' in recipe.keys() else ""
+    _image = ""
+    if 'images' in recipe.keys():
+        if isinstance(recipe['images'], dict):
+            _image = recipe['images']['url']
+        elif isinstance(recipe['images'], list):
+            _image = recipe['images'][0]
+        elif isinstance(recipe['images'], str):
+            _image = recipe['images']
+    image = "<img src=\"{}\">".format(_image) if _image else ""
     with open(filename, 'w') as f:
         f.write(
             scaffolding.format(
@@ -60,22 +72,33 @@ def _write_html(recipe, file = '', folder = None):
     print('HTML file created at {:s}'.format(filename))
     return filename
 
+
 def _open_html(html):
     webbrowser.open(html)
-    
+
+
 def _get_recipe_tag(parser):
     scripts = parser.find_all('script')
+    scripts = list(filter(lambda x: x is not None, scripts))
     for sc in scripts:
-        if 'yoast-schema-graph' in sc.get_attribute_list('class'):
+        if sc.string is not None and \
+           re.findall('"@type":\w?"Recipe"', sc.string):
             return sc
     raise RuntimeError('Couldn\'t find recipe tag in html.')
-    
+
+
 def _get_recipe_dict(tag):
-    _json = json.loads(tag.string)['@graph']
-    _recipe = list(filter(lambda x: x['@type'] == 'Recipe', _json))[0]
+    _json = json.loads(tag.string)
+    if isinstance(_json, dict) and _json['@type'] == 'Recipe':
+        _recipe = _json
+    else:
+        if isinstance(_json, dict) and '@graph' in _json.keys():
+            _json = _json['@graph']
+        _recipe = list(filter(lambda x: x['@type'] == 'Recipe', _json))[0]
     if not _recipe:
         raise RuntimeError('Couldn\'t find recipe dictionary in json.')
     return _recipe
+
 
 def _parse_recipe(_recipe):
     recipe = {
@@ -87,27 +110,26 @@ def _parse_recipe(_recipe):
     recipe['instructions'] = [x['text'] for x in recipe['instructions']]
     return recipe
 
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
-    #url
     parser.add_argument(
-        dest = 'url',
-        action = 'store',
-        type = str
+        dest='url',
+        action='store',
+        type=str
     )
     parser.add_argument(
         '--folder',
-        dest = 'folder',
-        action = 'store',
-        default = '/tmp'
+        dest='folder',
+        action='store',
+        default='/tmp'
     )
     parser.add_argument(
         '--file',
-        dest = 'file',
-        action = 'store',
-        default = None
+        dest='file',
+        action='store',
+        default=None
     )
-    
     args = parser.parse_args()
     main(args.url, args.folder, args.file)
